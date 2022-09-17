14253494cdab2ae040bf2988dbae070583c905e0
Arsh Ali Khan <95927424+arsh360@users.noreply.github.com>
2022-09-17 20:43:39 +0530

    Add files via upload

---
+++ InsertionSort/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {4,5,1,2,3};
+        Sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void Sort (int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+}
